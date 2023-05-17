78b5903c45613504ce3af5d1372a1148c708d990
abc740966 <lingjun.song@yahoo.com>
2023-05-16 20:30:15 -0500

    adding battery class

---
+++ battery/nubbin_battery.py
@@ -0,0 +1,7 @@
+class NubbinBattery:
+    def __init__(self, current_date, last_service_date):
+        self.__current_date = current date
+        self.__last_service_date = __last_service_date
+
+    def needs_service():
+        return self.__current_date - self.__last_service_date == 4
