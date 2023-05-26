4300f6807f746dd1393b690bc403e127febb2966
abc740966 <lingjun.song@yahoo.com>
2023-05-26 11:26:40 -0500

    update battery syntax

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -2,7 +2,7 @@ from battery.battery import Battery
 
 class NubbinBattery(Battery):
     def __init__(self, current_date, last_service_date):
-        self.__current_date = current date
+        self.__current_date = current_date
         self.__last_service_date = __last_service_date
 
     def needs_service():
