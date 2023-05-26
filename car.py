8c533415eabd037f6a9e7b1fccc9df7396294df3
abc740966 <lingjun.song@yahoo.com>
2023-05-26 11:30:41 -0500

    updated car class

--- car.py
+++ car.py
@@ -1,10 +1,5 @@
-from engine import Engine:
-from battery import Battery:
-
-class Serviceable(Car):
-
-    def needs_service(self, engine, battery):
-        return Car(engine, battery)
+from engine.engine import Engine
+from battery.battery import Battery
 
 class Car(Engine, Battery):
     def __init__(self, engine, battery):
@@ -12,5 +7,8 @@ class Car(Engine, Battery):
         self.__battery = Battery()
 
     def needs_service(self):
-        return self.engine.needs_service()
-        or self.battery.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service()
+
+class Serviceable(Car):
+    def needs_service(self, engine, battery):
+        return Car(engine, battery)
